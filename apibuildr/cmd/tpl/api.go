9d0f3988616ba2b9c29516cf894ec4f2a35b7c13
chandan <cskkman@gmail.com>
2022-02-23 16:11:22 +0530

    addApi command for get apis added

--- apibuildr/cmd/tpl/api.go
+++ apibuildr/cmd/tpl/api.go
@@ -5,15 +5,58 @@ func GetApiTemplate() []byte {
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/focks/apibuildr"
+	"net/http"
+)
+
+type {{ .Name }}ApiResponse struct {
+	Message string 
+}
+
+const {{ .Name }}Api = "{{ .Name }}Api"
+
+var {{.Name}}ApiHandler = apibuildr.ApiHandler{
+	Name:   {{ .Name }}Api,
+	Path:   "{{ .Path }}",
+	Method: http.MethodGet,
+	HandleFunc: func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		defer fmt.Println("api request ends")
+		fmt.Println("api request starts")
+		
+		res := {{ .Name }}ApiResponse{Message: "ok",}
+		bites, _ := json.Marshal(res)
+		
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(bites)
+
+	},
+}
+
+func init() {
+	helloApiHandler.RegisterToRouter(Router)
+}
+
+`)
+}
+
+func PostApiTemplate() []byte {
+	return []byte(`
+package cmd
+
+import (
 	"fmt"
 	"net/http"
 )
 
-const {{.Name}}Api = "{{.Name}}Api"
+const {{ .Name }}Api = "{{ .Name }}Api"
 
 var helloApiHandler = ApiHandler{
 	Name:   HelloApi,
-	Path:   "/v1/{{{.Path}}:{{.Path}}(?:\\/)?}",
+	Path:   "/v1/{{{ .Path }}:{{ .Path }}(?:\\/)?}",
 	Method: http.MethodGet,
 	HandleFunc: func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
@@ -32,8 +75,4 @@ func init() {
 }
 
 `)
-}
-
-func PostApiTemplate() []byte {
-	return []byte(``)
 }
