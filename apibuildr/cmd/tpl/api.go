3fe3481cdf63813223f596a4f8723268794704db
chandan <cskkman@gmail.com>
2022-02-23 15:47:31 +0530

    temp changes for add api get template

--- apibuildr/cmd/tpl/api.go
+++ apibuildr/cmd/tpl/api.go
@@ -1,7 +1,37 @@
 package tpl
 
 func GetApiTemplate() []byte {
-	return []byte(``)
+	return []byte(`
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const {{.Name}}Api = "{{.Name}}Api"
+
+var helloApiHandler = ApiHandler{
+	Name:   HelloApi,
+	Path:   "/v1/{{{.Path}}:{{.Path}}(?:\\/)?}",
+	Method: http.MethodGet,
+	HandleFunc: func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		defer fmt.Println("api request ends")
+		fmt.Println("api request starts")
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte())
+
+	},
+}
+
+func init() {
+	helloApiHandler.RegisterToRouter(Router)
+}
+
+`)
 }
 
 func PostApiTemplate() []byte {
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/focks/apibuildr
 go 1.17
 
 require (
+	github.com/gorilla/mux v1.8.0
+	github.com/pborman/uuid v1.2.1
+)
+
+require (
 	github.com/google/uuid v1.1.2 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/spf13/cobra v1.3.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
