efac29be61b78c1b62e9be6ef46f6ab64c155c88
chandan <cskkman@gmail.com>
2022-02-26 13:08:10 +0530

    test template for getapis added

--- apibuildr/cmd/tpl/ctrl.go
+++ apibuildr/cmd/tpl/ctrl.go
@@ -15,3 +15,70 @@ func {{ .Name }}Ctrl(ctx context.Context) (*apibuildr.OkResponse, *apibuildr.Api
 
 `)
 }
+
+func GetApiCtrlTestsTemplate() []byte {
+	return []byte(`
+package internal
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+var {{ .Name }}TestCases = map[string]struct {
+	description string
+	wantErr     bool
+
+	setupFunc func() error
+
+	testValidate func(err error) error
+
+	cleanupFunc func() error
+}{
+	"case 1 : base test case": {
+		description: "the base case",
+		wantErr:     false,
+		testValidate: func(err error) error {
+			return nil
+		},
+		setupFunc: func() error {
+			fmt.Println("setup complete")
+			return nil
+		},
+		cleanupFunc: func() error {
+
+			return nil
+
+		},
+	},
+}
+
+func Test{{ .Name }}Ctrl(t *testing.T) {
+
+	for testName, tt := range {{ .Name }}TestCases {
+		t.Run(testName, func(t *testing.T) {
+			if err := tt.setupFunc(); err != nil {
+				t.Error("error setting up test for ", testName)
+			}
+
+			ctx := context.Background()
+			response, err := {{ .Name }}Ctrl(ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("error = %v, wantErr %v", err, tt.wantErr)
+			}
+			_ = response
+
+			if e := tt.testValidate(err); e != nil {
+				t.Error(err)
+			}
+
+			if err := tt.cleanupFunc(); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
+
+`)
+}
