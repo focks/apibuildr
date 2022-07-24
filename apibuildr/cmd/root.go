b8d97431976b2f10c8e5963efcb9b62105b2dd86
focksh <focksh@chandans-MacBook-Air.local>
2022-07-24 21:46:10 +0530

    command description changed

--- apibuildr/cmd/root.go
+++ apibuildr/cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2022 focks cskkman@gmail.com
-
 */
 package cmd
 
@@ -13,16 +12,12 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "apibuildr",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "apibuildr generates boilerplate code for Rest REST API handlers",
+	Long: `apibuildr is a commandline tool for creating rest apis in golang language.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+apibuildr makes it easy to generate boilerplate code while adding rest apis.
+	
+It is not a framework, it uses gorilla mux server internally.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,13 +30,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.apibuildr.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
